a1ab1324dc194ac336df48554710d769f4d54ce5
madiwa Simon <mad.simon01@gmail.com>
2020-11-13 19:28:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   array = []
-  array << DON_G, JOELLE_VD
+  array << [DON_G, JOELLE_VD]
 end
 
 def aoh_lookup(aoh, row, key)
